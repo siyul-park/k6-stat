09cf5b56cb243a171bd21a32b266cfe4abdf14f6
siyual-park <siyual.bak@gmail.com>
2022-06-30 14:00:54 +0900

    fix: change to use y - x

--- lib/diff.ts
+++ lib/diff.ts
@@ -5,7 +5,7 @@ function diff(x: Summary, y: Summary): Summary {
   const state: Summary['state'] = {
     isStdOutTTY: y.state.isStdOutTTY,
     isStdErrTTY: y.state.isStdErrTTY,
-    testRunDurationMs: x.state.testRunDurationMs - y.state.testRunDurationMs,
+    testRunDurationMs: y.state.testRunDurationMs - x.state.testRunDurationMs,
   };
   const options: Summary['options'] = {
     noColor: y.options.noColor,
@@ -20,14 +20,18 @@ function diff(x: Summary, y: Summary): Summary {
     const xValues = x.metrics[metricKey]?.values ?? {};
     const yValues = y.metrics[metricKey]?.values ?? {};
 
+    const type = y.metrics[metricKey]?.type ?? x.metrics[metricKey]?.type ?? '';
+    const contains = y.metrics[metricKey]?.contains ?? x.metrics[metricKey]?.contains ?? '';
+
     const values: Record<string, number> = {};
     arrayUnion(Object.keys(xValues), Object.keys(yValues)).forEach((valueKey) => {
       values[valueKey] = (yValues[valueKey] ?? 0) - (xValues[valueKey] ?? 0);
     });
-    metrics[metricKey] = { 
+
+    metrics[metricKey] = {
       values,
-      type: y.metrics[metricKey]?.type ?? x.metrics[metricKey]?.type ?? '',
-      contains: y.metrics[metricKey]?.contains ?? x.metrics[metricKey]?.contains ?? '',
+      type,
+      contains,
     };
   });
 
